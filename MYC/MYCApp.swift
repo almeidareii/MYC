10516eec04f3bdf981bcde86153f31a26a45ccb5
almeidareii <reinaldoalmeida0702@hotmail.com>
2023-09-19 14:34:57 -0300

    Initial Commit

---
+++ MYC/MYCApp.swift
@@ -0,0 +1,17 @@
+//
+//  MYCApp.swift
+//  MYC
+//
+//  Created by Reinaldo Almeida on 19/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MYCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
